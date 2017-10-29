f07d645835e058ce9300e39cdb5869e238f0eb8f
Arnab Dey <arnabdey@lawn-128-61-37-212.lawn.gatech.edu>
2017-10-28 22:12:28 -0400

    Adding array

--- background.js
+++ background.js
@@ -3,13 +3,15 @@ var on = false, isAcademic = false;
 var academic = ["pearsonmylabandmastering",
 "portal.mypearson", "tophat", "github", "w3schools",
 "stackoverflow"];
-
+// var link = {website: "", academic : false, startTime : new Date(), endTime: new Date()};
+var history = new Array();
+var numOfLinks = 0;
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+        console.log(history);
         if (on === true) {
             console.log(request);
             startTime = new Date();
-            console.log(startTime);
             if (request.indexOf(".edu") > 0 ||
                 request.indexOf(".org") > 0 ||
                 request.indexOf(".gov") > 0) {
@@ -28,6 +30,11 @@ chrome.runtime.onMessage.addListener(
                 }
                 console.log("loop finished");
             }
+            var link = {website: request, academic : isAcademic, startTime : startTime, endTime : new Date()};
+            console.log(link);
+            history[numOfLinks] = link;
+            console.log(history);
+            numOfLinks++;
         }
         if (request === "on") {
             on = true;
