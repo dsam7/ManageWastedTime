b3ad76da1d4b50637ade2b0fd4f7d5489994d5eb
Arnab Dey <arnabdey@lawn-128-61-37-212.lawn.gatech.edu>
2017-10-28 21:39:54 -0400

    narrowing results for .edu

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 var startTime, endTime, diff;
-var on = false;
+var on = false, isAcademic = false;
+var academic = ["pearsonmylabandmastering",
+"portal.mypearson", "tophat", "github", "w3schools",
+"stackoverflow"];
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -7,10 +10,23 @@ chrome.runtime.onMessage.addListener(
             console.log(request);
             startTime = new Date();
             console.log(startTime);
-            function() {
-                if (request.indexOf(".edu") > 0) {
-                    conosle.log("academic");
+            if (request.indexOf(".edu") > 0 ||
+                request.indexOf(".org") > 0 ||
+                request.indexOf(".gov") > 0) {
+                isAcademic = true;
+                console.log("academic");
+            } else {
+                var found = false;
+                var counter = 0;
+                while (!found && counter < academic.length) {
+                    if (request.indexOf(academic[counter]) >= 0) {
+                        found = true;
+                        isAcademic = true;
+                        console.log("academic loop");
+                    }
+                    counter++;
                 }
+                console.log("loop finished");
             }
         }
         if (request === "on") {
